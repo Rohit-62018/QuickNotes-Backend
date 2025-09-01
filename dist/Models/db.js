a9ee1d300e72f03217f1c27d5a9267fd294b2849
Rohit <rohitkumargiddi@gmail.com>
2025-09-01 18:00:45 +0530

    fix: dependencies

--- dist/Models/db.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-const mongoose = require('mongoose');
-mongoose.connect(process.env.DB_URL)
-    .then(() => console.log("DB is connected"))
-    .catch((err) => console.log("MongoDB connection failed:", err));
